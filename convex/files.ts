a9fa60e354fd4e0c6369dff88ca9237ff1436e0a
r4j1025 <raj2505000@gmail.com>
2025-01-26 13:58:23 +0530

    initial setup completed

--- convex/files.ts
+++ convex/files.ts
@@ -1,13 +1,37 @@
-import { v } from "convex/values"
-import {mutation} from "./_generated/server"
+import { ConvexError, v } from "convex/values"
+import {
+    MutationCtx,
+    QueryCtx,
+    internalMutation,
+    mutation,
+    query,
+  } from "./_generated/server";
 
 export const createFile = mutation({
     args:{
         name:v.string(),
     },
     async handler(ctx, args) {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new ConvexError("you must be logged in to upload a file");
+        }
+
         await ctx.db.insert("files",{
           name: args.name,
         })
     },
 });
+
+export const getFiles = query({
+    args: {},
+    async handler(ctx, args){
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            return [];
+        }
+        return ctx.db.query('files').collect()
+    }
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import {  SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
   const createFile = useMutation(api.files.createFile);
+  const files = useQuery(api.files.getFiles);
+
   return (
     <main className="flex flex-col min-h-screen justify-between items-center">
       <SignedIn>
@@ -18,6 +20,11 @@ export default function Home() {
           <Button>Sign in</Button>
         </SignInButton>
       </SignedOut>
+
+      {files?.map((file)=>{
+        return <div key={file._id}>{file.name}</div>
+      })}
+
       <Button onClick={()=>{
         createFile({
           name: "hello world",
